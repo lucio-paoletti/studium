c96b068896a49480ed24883faeb789ec276081bc
lucio-paoletti <lucio.coder.paoletti@gmail.com>
2022-06-19 19:54:24 +0200

    feat(ng): added event emitter linked in different component

--- ng/cmp-databinding-start/src/app/app.component.ts
+++ ng/cmp-databinding-start/src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: "server", name: "TestServer", content: "just a test" },
   ];
+
+  onServerAdded(serverData : {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      name:serverData.serverName,
+      content: serverData.serverContent,
+      type: "server"
+    });
+  }
+
+  onBluePrintAdded(bluePrint: { serverName: string, serverContent: string }){
+    this.serverElements.push({
+      name: bluePrint.serverName,
+      content: bluePrint.serverContent,
+      type: "blueprint"
+    });
+  }
 }
