c661ec8e33f9a3b1ca85f7eaeafa47646cec46fd
lucio-paoletti <lucio.coder.paoletti@gmail.com>
2022-06-21 21:36:39 +0200

    feat(ng): added simple observables

--- ng/observable/src/app/app.module.ts
+++ ng/observable/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObsComponent } from './components/obs/obs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObsComponent
   ],
   imports: [
     BrowserModule,
